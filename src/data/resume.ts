32f020c3d166b812dd845a9d0600a3e16c3ea777
Stanley Akpama <akpamastanlee@gmail.com>
2023-07-09 04:45:40 +0100

    Update resume to v3.0.1

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -65,11 +65,11 @@ const workExperiences: WorkExperience[] = [
         startDate: 'APR 2022',
         endDate: 'NOV 2022',
         achievements: [
-            'Led development of OurPass business application enabling 4000+ businesses to receive online and offline payments.',
+            'Led development of OurPass business application enabling 5000+ businesses to receive online and offline payments.',
             'Collaborated with cross-functional team members in the development and implementation of EMV(Europay, Mastercard, and Visa standard) card payment processing and additional value-added services for OurPass point-of-sale application.',
             'Worked closely with product owners to identify, plan, and deliver new features, such as QR payments, point-of-sale integration, and improved onboarding and verification process.',
             'Implemented a scalable architecture and added automated tests to improve code quality, resulting in a reduction in bug reports and customer turnover (churn rate).',
-            'Set up and managed the CI/CD pipeline and deployments.',
+            'Set up and managed CI/CD pipeline and deployments.',
         ],
     },
     {
@@ -81,7 +81,7 @@ const workExperiences: WorkExperience[] = [
             'Managed a team of 4 people and led the development of a food delivery product for merchants and customers',
             'Developed a simple alerting system integrated with Slack which allowed us to distribute incident information to both customers and the operations team, increasing the efficiency of our day-to-day operations',
             'Implemented an image optimization pipeline that resizes, compresses, and caching images via CDN. Which increased app performance by 67%.',
-            'Set up and managed the CI/CD pipeline and deployments.',
+            'Set up and managed CI/CD pipeline and deployments.',
         ],
     },
     {
